372cfa7cecb156c7d67511244da384a458f733ee
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-02-07 14:37:20 -0700

    calculator

--- HobbyWebsite/Controllers/HomeController.cs
+++ HobbyWebsite/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace HobbyWebsite.Controllers
             return View();
         }
 
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- HobbyWebsite/wwwroot/js/site.js
+++ HobbyWebsite/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#btnSubmit").click(function () {
+    let hours = parseFloat($("#hours").val());
+    let rate = parseFloat($("#rate").val());
+
+    if (hours <= 0 || isNaN(hours)) {
+        $("#output").html("Please enter a valid number of hours.");
+    }
+    else {
+        let total = hours * rate;
+        $("#output").html("$" + total.toString());
+    }
+
+})
